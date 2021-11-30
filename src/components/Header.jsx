f5208273acc6dcdbadbc47ff5628a8066c7ec1b5
adham618 <adhamtarek291@gmail.com>
2021-11-30 20:54:36 +0200

    rename <Button text="add:/> to Add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ title }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="add" />
+      <Button color="green" text="Add" />
     </header>
   )
 }
