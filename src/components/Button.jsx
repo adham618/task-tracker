86f7bd4ddebd64b7e4a3909c7dadc0d153ee1e2e
adham618 <adhamtarek291@gmail.com>
2021-11-30 21:13:31 +0200

    complete Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,11 @@
-const Button = (props) => {
-  return <button className="btn">{props}</button>
 
+const Button = ({ color, text, onClick }) => {
+
+  return <button onClick={onClick} className="btn" style={{ backgroundColor: color }}>{text}</button>
+
+}
+Button.defaultProps = {
+  color: "steelblue"
 }
 
 export default Button
